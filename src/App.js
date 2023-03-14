67592ac9b30cda2a915ac72c1c718abb53943809
gabatk <gabriela.trybuszek@gmail.com>
2023-03-14 22:17:59 +0100

    Search form implemented.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AppHeader from './components/AppHeader';
+import SearchForm from './components/SearchForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App wrapper">
     <AppHeader />
+    <SearchForm />
     </div>
   );
 }
---
+++ src/components/SearchForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchForm.css';
+
+const SearchForm = () => {
+	return (
+		<form>
+			<input type={'text'} placeholder='Search...' className='search'></input>
+		</form>
+	);
+};
+
+export default SearchForm;
