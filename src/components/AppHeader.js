c9b64bc2dffcd95eda0a5848a2fc23d5af78a276
gabatk <gabriela.trybuszek@gmail.com>
2023-03-14 22:04:59 +0100

    Header element in progress.

---
+++ src/components/AppHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './AppHeader.css'
+
+const AppHeader = () => {
+    return (
+<div>
+    <h1 className="header">Picture Finder</h1>
+</div>
+    )
+}
+
+export default AppHeader;
